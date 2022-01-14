d0972a47f88250b8d7490a34bb5ffe87d8de2a8b
kolberg-dev <74478295+kolberg-dev@users.noreply.github.com>
2022-01-14 09:56:27 +0100

    Added react-router and some routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
+import { Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, GlobalStyles } from "./theme";
+
+import Landing from "./components/views/Landing";
+import Login from "./components/views/Login";
+import Nearby from "./components/views/Nearby";
+import Profile from "./components/views/Profile";
+import Search from "./components/views/Search";
+import Signup from "./components/views/Signup";
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <GlobalStyles />
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Nearby" element={<Nearby />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Search" element={<Search />} />
+        <Route path="/Signup" element={<Signup />} />
+      </Routes>
     </ThemeProvider>
   );
 }
